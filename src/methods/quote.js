246bbe3efff24332df550c0bfd6dc82655d6ff2b
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-29 00:07:34 -0700

    implement quote method

--- src/methods/quote.js
+++ src/methods/quote.js
@@ -1,5 +1,8 @@
 const getQuotes = (client, id) => {
-
+  const url = id ? `/quote/${id}` : '/quote';
+  return client.get(url)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 export { getQuotes };
