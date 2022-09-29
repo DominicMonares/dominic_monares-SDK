97848cf0bac7fbeafab1455425b381dfcd6f9822
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-28 22:35:17 -0700

    add export and change instance error handler

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * from './methods/character';
 import * from './methods/movie';
 import * from './methods/quote';
 
-const theOneAPI = (token) => {
+const theOne = (token) => {
   const client = axios.create({
     baseURL: baseURL,
     headers: {
@@ -15,23 +15,27 @@ const theOneAPI = (token) => {
     }
   })
     .then(res => res)
-    .catch(err => console.error('Error creating API instance: ', err));
+    .catch(err => { throw `Error creating API instance: ${err}` });
 
   return {
     books: () => await getBooks(client),
     book: (id) => await getBooks(client, id),
     bookChapters: (id) => await getBookChapters(client, id),
+
     movies: () => await getMovies(client),
     movie: (id) => await getMovies(client, id),
     movieQuotes: (id) => await getMovieQuotes(client, id),
+
     characters: () => await getCharacters(client),
     character: (id) => await getCharacters(client, id),
     characterQuotes: (id) => await getCharacterQuotes(client, id),
+
     quotes: () => await getQuotes(client),
     quote: (id) => await getQuotes(client, id),
+
     chapters: () => await getChapters(client),
     chapter: (id) => await getChapter(client, id)
   };
 };
 
-
+export default theOne;
