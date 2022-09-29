a03bcfdd2b28123b002ed2229931a280a75c86a6
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-29 00:08:07 -0700

    implement chapter method

--- src/methods/chapter.js
+++ src/methods/chapter.js
@@ -1,5 +1,8 @@
 const getChapters = (client, id) => {
-
+  const url = id ? `/chapter/${id}` : '/chapter';
+  return client.get(url)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 export { getChapters };
