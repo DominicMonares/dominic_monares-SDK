424297692a0f6f933010c325850c5fb894c02d8b
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-29 00:06:32 -0700

    implement character methods

--- src/methods/character.js
+++ src/methods/character.js
@@ -1,9 +1,14 @@
 const getCharacters = (client, id) => {
-
+  const url = id ? `/character/${id}` : '/character';
+  return client.get(url)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 const getCharacterQuotes = (client, id) => {
-
+  return client.get(`/character/${id}/quote`)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 export { getCharacters, getCharacterQuotes };
