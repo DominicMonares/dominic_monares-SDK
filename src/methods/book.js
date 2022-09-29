a2f3de08a2f1eb8ee6f3e4e85268b9f3126802ac
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-29 00:01:27 -0700

    implement book methods

--- src/methods/book.js
+++ src/methods/book.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
 const getBooks = (client, id) => {
-
+  const url = id ? `/book/${id}` : '/book';
+  return client.get(url)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 const getBookChapters = (client, id) => {
-
+  return client.get(`/book/${id}/chapter`)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 export { getBooks, getBookChapters };
