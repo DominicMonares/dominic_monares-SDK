4baedeb3522ce66e214d1aeaac5d1111b0c2855b
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-29 00:04:39 -0700

    implement movie methods

--- src/methods/movie.js
+++ src/methods/movie.js
@@ -1,9 +1,14 @@
 const getMovies = (client, id) => {
-
+  const url = id ? `/movie/${id}` : '/movie';
+  return client.get(url)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 const getMovieQuotes = (client, id) => {
-
+  return client.get(`/movie/${id}/quote`)
+    .then(res => res)
+    .catch(err => { throw err });
 }
 
 export { getMovies, getMovieQuotes };
